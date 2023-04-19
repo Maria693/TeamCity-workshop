1fa18c69a301d97aa51e226015ac7cb876968fc0
Maria Egorova <ab-ermakova@yandex.ru>
2023-04-19 16:12:13 +0200

    basic test

---
+++ TeamCity-tests-workshow/src/main/java/org/workshop/api/models/Project.java
@@ -0,0 +1,15 @@
+package org.workshop.api.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class Project {
+    private ParentProject parentProject;
+    private String name;
+    private String id;
+    private boolean copyAssociatedSettings;
+
+}
